c94f4e36df3d14e537fdf16e356afe11793e0ba5
Sega Diop <diop.sega@econostic.net>
2019-05-19 21:18:45 +0200

    release_1_user_accounts - Implement Service Layer (user service authentication and token generation) : test ok

---
+++ src/main/java/com/kata/cubbyhole/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.kata.cubbyhole.service.impl;
+
+import com.kata.cubbyhole.security.jwt.JwtTokenProvider;
+import com.kata.cubbyhole.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider tokenProvider;
+
+
+    @Autowired
+    public UserServiceImpl(AuthenticationManager authenticationManager, JwtTokenProvider tokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;
+
+    }
+
+    @Override
+    public Authentication authenticate(String usernameOrEmail, String password) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usernameOrEmail, password)
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return authentication;
+    }
+
+    @Override
+    public String generateToken(Authentication authentication) {
+        return tokenProvider.generateToken(authentication);
+    }
+}
--- src/test/java/com/kata/cubbyhole/service/UserServiceTest.java
+++ src/test/java/com/kata/cubbyhole/service/UserServiceTest.java
@@ -3,12 +3,17 @@ package com.kata.cubbyhole.service;
 import com.kata.cubbyhole.runner.SpringJUnitParams;
 import com.kata.cubbyhole.security.jwt.JwtTokenProvider;
 import com.kata.cubbyhole.security.mapper.UserPrincipal;
+import com.kata.cubbyhole.service.impl.UserServiceImpl;
 import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -26,24 +31,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 public class UserServiceTest {
 
-    @Autowired
     private UserService userService;
 
     @Autowired
+    @Mock
     private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    @Mock
+    private AuthenticationManager authenticationManager;
 
     private PodamFactory podamFactory;
 
     @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
+        userService = new UserServiceImpl(authenticationManager, jwtTokenProvider);
         podamFactory = new PodamFactoryImpl();
     }
 
 
     @Test
-    @Parameters({"azerty", "azerty"})
+    @Parameters({"azerty, azerty"})
     public void should_authenticate_user_from_username_or_email_and_password(String usernameOrEmail, String password) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                usernameOrEmail,
+                password,
+                Collections.singletonList(new SimpleGrantedAuthority(ROLE_USER.name()))
+        );
+        Mockito.doReturn(usernamePasswordAuthenticationToken)
+                .when(authenticationManager)
+                .authenticate(Mockito.any(Authentication.class));
+
         Authentication authentication = userService.authenticate(usernameOrEmail, password);
+
         assertThat(authentication.isAuthenticated()).isTrue();
     }
 
