0a613bd15b6077f55350c82dc2680c9adf0da93b
Sega Diop <diop.sega@econostic.net>
2019-05-20 03:00:52 +0200

    release_1_user_accounts - correct Role repository : test ok

--- src/main/java/com/kata/cubbyhole/repository/RoleRepository.java
+++ src/main/java/com/kata/cubbyhole/repository/RoleRepository.java
@@ -3,9 +3,11 @@ package com.kata.cubbyhole.repository;
 import com.kata.cubbyhole.model.Role;
 import com.kata.cubbyhole.model.enumeration.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(RoleName roleName);
 }
